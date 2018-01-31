f8cce46fd8ab763ac50df2af86f484c1e661b8bf
rastapezzolo <rastapezzolo86@gmail.com>
2018-01-31 14:54:33 +0100

    Updated SvgFactory
    tolto .svg

--- src/SvgFactory.php
+++ src/SvgFactory.php
@@ -102,7 +102,7 @@ class SvgFactory
     public function getSvg($name)
     {
         return $this->svgCache->get($name, function () use ($name) {
-            return $this->svgCache[$name] = trim($this->files->get(sprintf('%s/%s.svg', rtrim($this->svgPath()), str_replace('.', '/', $name))));
+            return $this->svgCache[$name] = trim($this->files->get(sprintf('%s/%s', rtrim($this->svgPath()), $name)));
         });
     }
 }
